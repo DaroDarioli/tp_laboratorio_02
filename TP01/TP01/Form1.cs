526dc2a66f8f4a97fdcf59995387347441685157
DaroDarioli <darioesteban21@hotmail.com>
2017-04-27 21:41:32 -0300

    cargadoComoCarpeta
    cargadoComoCarpeta

---
+++ TP01/TP01/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP01
+{
+    public partial class Form1 : Form
+    {
+        Calculadora calculadora;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            calculadora = new Calculadora();
+
+            Numero numero1 = new Numero(this.txtNumero1.Text);
+            Numero numero2 = new Numero(this.txtNumero2.Text);         
+                     
+            lblResultado.Visible =  true;
+
+
+            try
+            {
+                lblResultado.Text = calculadora.operar(numero1, numero2, cmbOperacion.SelectedItem.ToString()).ToString();
+            }
+            catch
+            {
+                lblResultado.Text = "0";
+            
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {        
+                  
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+            lblResultado.Visible = false;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Visible = false;
+            cmbOperacion.SelectedItem = null;
+
+
+        }
+    }
+}
---
+++ TP01/TP01/Numero.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP01
+{
+    public class Numero
+    {
+        private double numero; 
+
+
+        /// <summary>
+        /// Crea espacio en memoria e inicializa el atributo numero de la clase Numero en 0
+        /// </summary>
+        public Numero()
+        {
+            this.numero = 0;    
+        }
+        /// <summary>
+        /// Crea espacio en memoria e inicializa el atributo numero de la clase Numero con el valor que se le pasa como string
+        /// </summary>
+        /// <param name="strNumero"></param>
+        public Numero(string strNumero)
+        {
+            double aux = 0;
+            
+            double.TryParse(strNumero, out aux); 
+               
+            this.numero = aux;
+        }
+
+        /// <summary>
+        /// Retorna el atributo del tipo double numero de la clase Numero
+        /// </summary>
+        /// <returns> el atributo del tipo double</returns>
+
+        public double getNumero()
+        {
+            return this.numero;
+        }
+
+        /// <summary>
+        /// Recibe un dato del tipo double y lo carga en el atributo numero de la clase Numero
+        /// </summary>
+        /// <param name="dblNumero"></param>
+        public Numero(double dblNumero)
+        {
+            this.numero = dblNumero;
+        }
+
+
+        /// <summary>
+        /// Recibe una cadena de caracteres y si es doble la carga al atributo numero de la clase Numero
+        /// </summary>
+        /// <param name="strNumero"></param>
+        private void setNumero(string strNumero)
+        {
+            this.numero = validarNumero(strNumero);
+        }
+
+
+        /// <summary>
+        /// Recibe un string, lo valida y si es sólo numérico lo devuelve como double
+        /// </summary>
+        /// <param name="strNumero">Una cadena de carctéres</param>
+        /// <returns>La cadena de caractéres convertida en double, o 0 si no pudo realizar la conversión</returns>
+        private double validarNumero(string strNumero)
+        {
+            double aux = 0;
+
+            double.TryParse(strNumero, out aux);
+
+            return aux;
+        }   
+
+       
+    }
+}
